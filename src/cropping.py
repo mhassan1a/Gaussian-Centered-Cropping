1fe626120f63ca119cefbdc621f4b7284cac8be3
Mohamed Hassan <mmabdelkader@outlook.com>
2024-04-05 17:18:31 +0200

    adding test_gaussian_cropping.py

--- src/cropping.py
+++ src/cropping.py
@@ -48,7 +48,35 @@ class GaussianCrops:
         Returns:
         - List of np.arrays: with two views of the cropped image.     
         """
-        image_width, image_height = img.shape[1], img.shape[0]
+        try:
+            assert 0 < self.crop_percentage < 1
+        except AssertionError:
+            raise ValueError("The crop percentage must be between 0 and 1.")
+        try:
+            assert self.std_scale > 0
+        except AssertionError:
+            raise ValueError("The standard deviation scale must be greater than 0.")
+        try:
+            assert self.min_max[0] < self.min_max[1]
+        except AssertionError:
+            raise ValueError("The minimum value must be less than the maximum value.")
+        try:
+            assert 0 <= self.min_max[0] <= 1 and 0 <= self.min_max[1] <= 1
+        except AssertionError:
+            raise ValueError("The minimum and maximum values must be between 0 and 1.")
+        try:
+            img.ndim == 3
+        except AssertionError:
+            raise ValueError("The image must be a 3-dimensional array.")
+        
+        if img.shape[0] == 3: # C x H x W
+            image_height, image_width = img.shape[1], img.shape[2]
+        elif img.shape[2] == 3: # H x W x C    
+            image_height, image_width = img.shape[0], img.shape[1]
+        else:
+            raise ValueError("The image must have 3 channels.")
+        
+            
         crop_width = int(image_width * np.sqrt(self.crop_percentage))
         crop_height = int(image_height * np.sqrt(self.crop_percentage))
         
@@ -148,141 +176,7 @@ class GaussianCrops:
             assert cropped_image.shape[1] == crop_width         
             crops.append(cropped_image)
         return crops
-    
-class UniformCrops:
-    def __init__(self, min_scale=-1, max_scale =1, crop_percentage=0.4, seed=None,
-                padding=False, regularised_crop=False):
-        """Initialization method for the cropping class.
-
-            Args:
-                min_scale (int, optional): Minimum scale factor for cropping. Defaults to -1.
-                max_scale (int, optional): Maximum scale factor for cropping. Defaults to 1.
-                crop_percentage (float, optional): Percentage of the image to crop. Defaults to 0.4.
-                seed (int, optional): Seed for random number generation. Defaults to None.
-                padding (bool, optional): Boolean flag indicating whether padding is applied. Defaults to False.
-                regularised_crop (bool, optional): Boolean flag indicating whether regularized cropping is applied. Defaults to False.
-        """
-        
-        self.crop_percentage = crop_percentage
-        self.seed = seed
-        self.padding = padding
-        self.regularised_crop = regularised_crop
-        self.min_scale = min_scale
-        self.max_scale = max_scale
-        
-    
-    def ucc(self, img):
-        """
-        Uniform centered Cropping 
-        Crop an image using isotropic uniform distributed cropping.
-        Parameters:
-        - image (np.array): Image to be cropped.
-        - crop_percentage (float): Percentage of the original image to be cropped.
-        - seed (int): Seed for reproducibility.
-        
-        Returns:
-        - List of np.arrays: with two views of the cropped image.     
-        """
-        assert image.ndim == 3 and image.shape[2] == 3
-        image_width, image_height = img.shape[1], img.shape[0]
-        crop_width = int(image_width * np.sqrt(self.crop_percentage))
-        crop_height = int(image_height * np.sqrt(self.crop_percentage))
-        
-        min_hight = image_height//2 - int(image_width * self.min_scale)
-        max_hight=  image_height//2 + int(image_width * self.max_scale)
-        
-        min_width = image_width//2 - int(image_height * self.min_scale)
-        max_width = image_width//2 + int(image_height * self.max_scale)
-        
-        if self.seed is not None:
-            np.random.seed(self.seed)
-        centers = np.random.uniform([image_width / 2, image_height / 2],
-                                    [[min_hight, min_width], [max_hight, max_width]], (2, 2))
-        crops = []
-        for center in centers:
-            center_x, center_y = int(center[0]), int(center[1])
-            left = center_x - crop_width // 2
-            top =  center_y - crop_height // 2
-            bottom = top + crop_height 
-            right = left + crop_width 
-            
-           
-            if self.regularised_crop:
-                if top < 0:
-                    top = 0
-                    bottom = crop_height
-                if left < 0:
-                    left = 0
-                    right = crop_width
-                if bottom > image_height:
-                    bottom = image_height
-                    top = bottom - crop_height
-                if right > image_width:
-                    right = image_width
-                    left = right - crop_width             
-                   
-       
-            if self.padding:
-                if left < 0 and right> 0:
-                    left = 0
-                    pad_left = crop_width - right
-                    pad_right = 0
-                elif right > image_width and left < image_width:
-                    right = image_width
-                    pad_left = 0
-                    pad_right = right - image_width
-                elif left <= 0 and right <=0:
-                    left = 0
-                    right = 0
-                    pad_left = crop_width
-                    pad_right = 0 
-                elif left >= image_width and right >= image_width:
-                    left = 0
-                    right = 0
-                    pad_left = 0
-                    pad_right = crop_width                   
-                else:
-                    pad_left = 0
-                    pad_right = 0
-                    
-                if top < 0 and bottom > 0:
-                    top = 0
-                    pad_top = crop_height - bottom
-                    pad_bottom = 0
-                elif bottom > image_height and top < image_height:
-                    bottom = image_height
-                    pad_top = 0
-                    pad_bottom = bottom - image_height
-                elif top <= 0 and bottom <= 0:
-                    top = 0
-                    bottom = 0
-                    pad_top = crop_height
-                    pad_bottom = 0
-                elif top >= image_height and bottom >= image_height:
-                    top = 0
-                    bottom = 0
-                    pad_top = 0
-                    pad_bottom = crop_height
-                else:
-                    pad_top = 0
-                    pad_bottom = 0
-                 
-            if bottom - top < crop_height:
-                pad_top = (crop_height - (bottom - top)) // 2
-                pad_bottom = crop_height - (bottom - top) - pad_top   
-            if right - left < crop_width:
-                pad_left = (crop_width - (right - left)) // 2
-                pad_right = crop_width - (right - left) - pad_left
-                    
-            cropped_image = img[top:bottom, left:right]
-            cropped_image = np.pad(cropped_image, ((pad_top, pad_bottom),
-                (pad_left, pad_right), (0, 0)), mode='constant')
-             
-            assert cropped_image.shape[0] == crop_height
-            assert cropped_image.shape[1] == crop_width         
-            crops.append(cropped_image)
-        return crops
-    
+  
     
 if __name__ == "__main__":
     img = Image.open("g44yy1dz.bmp")
